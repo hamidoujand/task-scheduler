410ecacb4aee1d267d54e351d17ee3518f5f3de6
Hamid Oujand <hmdrzadev@gmail.com>
2024-07-16 21:31:41 +0330

    setup configuration parsing

--- app/services/scheduler/api/main.go
+++ app/services/scheduler/api/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/hamidoujand/task-scheduler/foundation/logger"
 )
 
@@ -12,8 +15,41 @@ import (
 var build = "development"
 
 func main() {
-	isProd := os.Getenv("ENV") == "production"
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "err: %s", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	//setup configurations
+	configs := struct {
+		API struct {
+			Host            string        `conf:"default:0.0.0.0:8000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			Environment     string        `conf:"default:development"`
+		}
+	}{}
+
+	prefix := "SCHEDULER"
+	if help, err := conf.Parse(prefix, &configs); err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		//some error we need to handle
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	cfg, err := conf.String(&configs)
+	if err != nil {
+		return fmt.Errorf("stringify configs: %w", err)
+	}
 	//setup logger
+	isProd := configs.API.Environment == "production"
+
 	attrs := []slog.Attr{
 		{Key: "build", Value: slog.StringValue(build)},
 		{Key: "app", Value: slog.StringValue("task-scheduler")},
@@ -21,14 +57,7 @@ func main() {
 
 	logger := logger.NewCustomLogger(slog.LevelInfo, isProd, attrs...)
 
-	if err := run(logger); err != nil {
-		fmt.Fprintf(os.Stderr, "err: %s", err)
-		os.Exit(1)
-	}
-}
-
-func run(logger *slog.Logger) error {
-	logger.Info("startup", "Hello", "World")
+	logger.Info("current app configurations", "configs", cfg)
 
 	return nil
 }
--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 run:
-	ENV=production go run app/services/scheduler/api/main.go
+	SCHEDULER_API_ENVIRONMENT=development go run app/services/scheduler/api/main.go 
+
+tidy:
+	go mod tidy 
+	go mod vendor 	
