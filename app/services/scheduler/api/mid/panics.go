53e98ce392e4393928dab1baa4decdd7a12656f3
Hamid Oujand <hmdrzadev@gmail.com>
2024-07-19 15:36:53 +0330

    add panics handler middleware

---
+++ app/services/scheduler/api/mid/panics.go
@@ -0,0 +1,29 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/hamidoujand/task-scheduler/app/services/scheduler/api/errs"
+	"github.com/hamidoujand/task-scheduler/foundation/web"
+)
+
+func Panics() web.Middleware {
+	m := func(h web.Handler) web.Handler {
+		handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					//need to capture and return error to "error handler mid"
+					stackTrace := debug.Stack()
+					err = errs.NewAppErrorf(http.StatusInternalServerError, "PANIC[%v] STACK[%s]", r, string(stackTrace))
+				}
+			}()
+
+			return h(ctx, w, r)
+		}
+
+		return handler
+	}
+	return m
+}
