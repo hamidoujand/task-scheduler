6822e04a5e2c7f730a19460bbb830ce33209a562
Hamid Oujand <hmdrzadev@gmail.com>
2024-07-25 20:23:38 +0330

    change store into postgres package

---
+++ business/domain/task/store/postgres/models.go
@@ -0,0 +1,61 @@
+package postgres
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hamidoujand/task-scheduler/business/domain/task"
+)
+
+// Task represents a task object inside of database.
+type Task struct {
+	Id           string
+	Command      string
+	Args         []string
+	Status       string
+	Result       sql.Null[string]
+	ErrorMessage sql.Null[string]
+	ScheduledAt  time.Time
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+func toDBTask(t task.Task) Task {
+	return Task{
+		Id:           t.Id.String(),
+		Command:      t.Command,
+		Args:         t.Args,
+		Status:       t.Status,
+		Result:       sql.Null[string]{V: t.Result, Valid: t.Result != ""},
+		ErrorMessage: sql.Null[string]{V: t.ErrMessage, Valid: t.ErrMessage != ""},
+		ScheduledAt:  t.ScheduledAt.UTC(),
+		CreatedAt:    t.CreatedAt.UTC(),
+		UpdatedAt:    t.ScheduledAt.UTC(),
+	}
+}
+
+func (t Task) toDomainTask() task.Task {
+	result := ""
+
+	if t.Result.Valid {
+		result = t.Result.V
+	}
+	errMsgs := ""
+	if t.ErrorMessage.Valid {
+		errMsgs = t.ErrorMessage.V
+	}
+
+	return task.Task{
+		//must parse since we taking it out of db.
+		Id:          uuid.MustParse(t.Id),
+		Command:     t.Command,
+		Args:        t.Args,
+		Status:      t.Status,
+		Result:      result,
+		ErrMessage:  errMsgs,
+		ScheduledAt: t.ScheduledAt.In(time.Local),
+		CreatedAt:   t.CreatedAt.In(time.Local),
+		UpdatedAt:   t.UpdatedAt.In(time.Local),
+	}
+}
---
+++ business/domain/task/store/postgres/store.go
@@ -0,0 +1,129 @@
+package postgres
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/hamidoujand/task-scheduler/business/database/postgres"
+	"github.com/hamidoujand/task-scheduler/business/domain/task"
+)
+
+// Store represents apis used to interact with database.
+type repository struct {
+	client *postgres.Client
+}
+
+// New creates a new store that uses *postgres.Client as its db client
+func NewRepository(clint *postgres.Client) *repository {
+	return &repository{
+		client: clint,
+	}
+}
+
+func (s *repository) Create(ctx context.Context, task task.Task) error {
+	const q = `
+	INSERT INTO tasks
+		(id,command,args,status,result,error_msg,scheduled_at,created_at,updated_at)
+	VALUES
+		($1,$2,$3,$4,$5,$6,$7,$8,$9);
+	`
+
+	dbTask := toDBTask(task)
+	_, err := s.client.DB.ExecContext(ctx, q,
+		dbTask.Id,
+		dbTask.Command,
+		dbTask.Args,
+		dbTask.Status,
+		dbTask.Result,
+		dbTask.ErrorMessage,
+		dbTask.ScheduledAt,
+		dbTask.CreatedAt,
+		dbTask.UpdatedAt,
+	)
+	if err != nil {
+		return fmt.Errorf("exec context: %w", err)
+	}
+	return nil
+}
+
+func (s *repository) Update(ctx context.Context, task task.Task) error {
+	const q = `
+	UPDATE 
+		tasks
+	SET
+		status =    $1,
+		result =    $2,
+		error_msg = $3
+	WHERE
+		id = $4		
+	`
+	dbTask := toDBTask(task)
+
+	_, err := s.client.DB.ExecContext(ctx, q,
+		dbTask.Status,
+		dbTask.Result,
+		dbTask.ErrorMessage,
+		dbTask.Id,
+	)
+	if err != nil {
+		return fmt.Errorf("exec context: %w", err)
+	}
+
+	return nil
+}
+func (s *repository) Delete(ctx context.Context, task task.Task) error {
+	const q = `
+	DELETE FROM
+		tasks
+	WHERE 
+		id = $1	
+	
+	`
+	_, err := s.client.DB.ExecContext(ctx, q, task.Id)
+	if err != nil {
+		return fmt.Errorf("exec context: %w", err)
+	}
+	return nil
+}
+func (s *repository) GetById(ctx context.Context, taskId uuid.UUID) (task.Task, error) {
+	var dbTask Task
+	const q = `
+	SELECT 
+		id, command,array_to_json(args) as args,status,result,error_msg,scheduled_at,created_at,updated_at
+	FROM 
+		tasks
+	WHERE 
+		id = $1		
+	`
+
+	row := s.client.DB.QueryRowContext(ctx, q, taskId.String())
+
+	var commandArgs any
+
+	if err := row.Scan(
+		&dbTask.Id,
+		&dbTask.Command,
+		&commandArgs,
+		&dbTask.Status,
+		&dbTask.Result,
+		&dbTask.ErrorMessage,
+		&dbTask.ScheduledAt,
+		&dbTask.CreatedAt,
+		&dbTask.UpdatedAt,
+	); err != nil {
+		return task.Task{}, fmt.Errorf("row scan: %w", err)
+	}
+
+	switch val := commandArgs.(type) {
+	case []byte:
+		if err := json.Unmarshal(val, &dbTask.Args); err != nil {
+			return task.Task{}, fmt.Errorf("unmarshalling args: %w", err)
+		}
+	default:
+		return task.Task{}, fmt.Errorf("args scan: %T", commandArgs)
+	}
+
+	return dbTask.toDomainTask(), nil
+}
