d082b8f8a2b921e2a9cb1cd328f7e27ba9f6ccab
Hamid Oujand <hmdrzadev@gmail.com>
2024-08-10 14:39:34 +0330

    remove worker domain

--- business/domain/worker/worker.go
+++
@@ -1,6 +0,0 @@
-// Package worker provides functionality to execute commands.
-package worker
-
-// Service represents all of the required APIs to execute commands by workers.
-type Service struct {
-}
